a3dae74e7b884e3df6bd0cac187c6900c75569cd
Khoa <52121010+11Khoa@users.noreply.github.com>
2020-06-14 22:10:04 +0700

    11

---
+++ index.js
@@ -0,0 +1,20 @@
+// alert("abc")
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  function setRandomColor() {
+      var item=document.querySelectorAll("li")
+      item.forEach(element => {
+          element.style.backgroundColor=getRandomColor();
+      });
+      
+  }
+  setRandomColor()
+
+
+  
