4df4355ff36abf3b4d4643191cb13c8340501908
Khoa <52121010+11Khoa@users.noreply.github.com>
2020-06-14 23:12:41 +0700

    color

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
-  function setRandomColor() {
-      var item=document.querySelectorAll("li")
-      item.forEach(element => {
-          element.style.backgroundColor=getRandomColor();
-      });
-      
-  }
-  setRandomColor()
+}
+function setRandomColor() {
+    var item = document.querySelectorAll("li")
+    item.forEach(element => {
+        element.style.backgroundColor = getRandomColor();
+    });
 
-
-  
+}
+setRandomColor()
+setInterval(() => {
+    setRandomColor()
+}, 5000);
