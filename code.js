d8673ac42500ac61904b86a09c1c3e0cd606d2ea
Khoa <52121010+11Khoa@users.noreply.github.com>
2020-11-06 13:34:08 +0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,15 @@
+// SET KEY
+var keykhoa = new Array();
+// END SET KEY
+var bienkiemtra = 0;
+for( i=0 ; i<keykhoa.length ; i++ ){
+	if(window.location.href.indexOf(keykhoa[i]) > -1) {
+		bienkiemtra = 1;
+		break;
+	}
+}
+if(bienkiemtra == 0){
+	jQuery('html').remove();
+	//window.location.replace("https://webminhthuan.com/thiet-ke-web.html");
+}
+//jQuery('.primarykey').remove();
